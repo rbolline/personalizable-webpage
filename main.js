d081e1b65345a72ffe05d181dfa40278c6abe767
Rohith Bollineni <Rohith@rojass-MacBook-Air.local>
2019-09-11 15:07:24 -0700

    added button action function frames

--- main.js
+++ main.js
@@ -10,13 +10,44 @@
   */
   function main() {
     resumeAction();
+    buttonActions();
   }
 
   /**
   * starts the battle mode between the pokemons
   */
+
   function resumeAction(){
     id("start-btn").addEventListener("click", openResume);
+  }
+  function buttonActions(){
+    var upbtn = document.getElementById("upbutton");
+    var downbtn = document.getElementById("downbutton");
+
+    upbtn.addEventListener("click", function(e){
+      /*if y at 0 in first div, do nothing
+        else if in first div, move to 0
+        else move up to previous div*/
+    });
+    upbtn.addEventListener("mouseover", function(e){
+      //set color to dark grey
+    });
+    upbtn.addEventListener("mouseout", function(e){
+      //set color to black
+    });
+    downbtn.addEventListener("click", function(e){
+      /*if y at bottom in last div, do nothing
+        else if in last div, move to bottom
+        else move down to next div*/
+    });
+    downbtn.addEventListener("mouseover", function(e){
+      //set color to dark grey
+    });
+    downbtn.addEventListener("mouseout", function(e){
+      //set color to black
+    });
+
+
   }
 
   /**
